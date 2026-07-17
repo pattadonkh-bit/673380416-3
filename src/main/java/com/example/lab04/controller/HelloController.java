69ece4f41dd15947001326cbc0156b9a026c5df3
Prayfon Hokseng <prayfon.h@kkumail.com>
2026-07-17 15:14:14 +0700

    Update HelloController.java

--- src/main/java/com/example/lab04/controller/HelloController.java
+++ src/main/java/com/example/lab04/controller/HelloController.java
@@ -28,9 +28,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     // ── ข้อมูลนักศึกษา ── แก้ไขตรงนี้ ──────────────────────────────────────
-    private static final String NAME    = "Your Name Here";     // TODO: ใส่ชื่อจริง
-    private static final String ID      = "xxxx";               // TODO: ใส่รหัสนักศึกษา
-    private static final String SECTION = "x";                  // TODO: ใส่เลข Section
+    private static final String NAME    = "พัทธดนย์ คำนัน";     // TODO: ใส่ชื่อจริง
+    private static final String ID      = "673380416-3";               // TODO: ใส่รหัสนักศึกษา
+    private static final String SECTION = "4";                  // TODO: ใส่เลข Section
     // ─────────────────────────────────────────────────────────────────────────
 
     /**
